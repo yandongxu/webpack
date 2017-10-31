53b2c46db5be7d51e2ee8334d6354f785e09dc23
dongxu <nextocean@gmail.com>
2017-10-31 18:34:12 +0800

    update

--- template/test/unit/specs/Hello.spec.js
+++ template/test/unit/specs/Hello.spec.js
@@ -1,20 +1,28 @@
 import Vue from 'vue';
+import store from '@/store';
 import Hello from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('should render correct contents', () => {
-    const Constructor = Vue.extend(Hello);
-    const vm = new Constructor({
-      propsData: {
-        count: 10
-      }
+    // Clean component
+    // const Constructor = Vue.extend(Hello);
+    // const vm = new Constructor({
+    //   propsData: {
+    //     count: 10
+    //   }
+    // }).$mount();
+
+    // Vuex deps
+    const vm = new Vue({
+      store,
+      render: h => h(Hello)
     }).$mount();
 
-    expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App');
+    expect(vm.$el.querySelector('h1').textContent)
+    .to.equal('Welcome to Your Vue.js App');
 
-    expect(vm.$el.querySelector('.hello h2').textContent)
-      .to.equal('10');
+    expect(vm.$el.querySelector('h2').textContent)
+      .to.equal('0');
 
     vm.$destroy();
   });
